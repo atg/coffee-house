1ba14da027d381ddb5c367636d164b27c67c37f6
David Czihak <me@davidczihak.com>
2009-01-19 17:16:13 +0100

    Ajax search basic added

--- application/views/inventory.php
+++ application/views/inventory.php
@@ -3,15 +3,17 @@
 
 <hr/>
 <div class="list">
-	<?php
-		foreach ($this->core->inventory() as $item)
-		{
-		    # 0 = ID
-		    # 1 = Type
-		    # 2 = Title
-		    # 3 = Description
-		    echo "<span class='{$item[1]}'>" . anchor('sugar/'.$item[0], $item[2]) . anchor('quickload/'.$item[0], 'Download', array('class'=>'quickload')) . "<div class='clear'></div></span>";
-		}
-	?>
+	<div id="list_content">
+		<?php
+			foreach ($this->core->inventory() as $item)
+			{
+			    # 0 = ID
+			    # 1 = Type
+			    # 2 = Title
+			    # 3 = Description
+			    echo "<span class='{$item[1]}'>" . anchor('sugar/'.$item[0], $item[2]) . anchor('quickload/'.$item[0], 'Download', array('class'=>'quickload')) . "<div class='clear'></div></span>";
+			}
+		?>
+	</div>
 </div>
 <hr/>
--- js/core.js
+++ js/core.js
@@ -1,5 +1,18 @@
 window.addEvent('domready',function(){
 
-	// code here
+	var searchField = $$('#search_field');
+	
+	var Search = new Request({
+		method: 'post',
+		url: 'http://www.fileability.com/coffee-house/index.php/ajax/search/',
+		onSuccess: function(responseText) {
+			$('content').set('html', responseText);
+		}
+	});
+	
+	searchField.addEvent('keydown', function(el){
+		var term = searchField.get('value');
+		Search.send('term='.term);
+	});
 
 });
